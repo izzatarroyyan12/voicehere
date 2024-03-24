24993008b95f718593ccdbfccecbaf2e24468cdb
izzatarroyyan12 <izzatarroyyan12@gmail.com>
2024-03-24 21:01:16 +0700

    feat: slice authentication page for smaller screens

--- frontend/src/app/(auth)/login/page.jsx
+++ frontend/src/app/(auth)/login/page.jsx
@@ -4,8 +4,7 @@ import { FcGoogle } from 'react-icons/fc';
 
 export default function Login() {
   return (
-    <div className="flex flex-col gap-y-2 items-center justify-center">
-      <Image src={VoicehereLogo} alt="voicehere logo" />
+<div className="flex flex-col gap-y-2 items-center justify-center shadow-2xl p-10 rounded-lg hover:shadow-md bg-white bg-opacity-5 border border-collapse sm:shadow-none sm:p-0 sm:rounded-none sm:hover:shadow-none sm:bg-transparent sm:bg-opacity-100 sm:border-none">      <Image src={VoicehereLogo} alt="voicehere logo" />
       <span className="font-semibold">here to help</span>
       <a
         href="/login"
