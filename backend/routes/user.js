9b80230aad2895a39817b8ce737390c1ae8ffbfa
izzatarroyyan12 <izzatarroyyan12@gmail.com>
2024-05-12 13:18:51 +0700

    fix file naming

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,11 +1,11 @@
 // userRoutes.js
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/userController');
+const userController = require('../controllers/user');
 const { verifyToken, verifyAdmin } = require('../middlewares/auth');
 
 // Define routes
-router.post('/register', userController.registerUser); // Register a new user
+router.post('/', userController.registerUser); // Register a new user
 router.post('/login', userController.loginUser); // User login
 router.post('/logout', verifyToken, userController.logoutUser); // User logout
 
