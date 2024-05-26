3e7900da5e51bad4edc0328e6f38ef3ae04df672
heintzz <mhasnanr@gmail.com>
2024-05-26 23:07:09 +0700

    feat: add delete endpoint

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,28 +1,31 @@
-//user.js model
 const { DataTypes } = require('sequelize');
-const db = require('../dbconnect'); // Import your database connection
+const sequelize = require('../dbconnect'); // Import your database connection
 
-const User = db.define('User', {
-  user_id: {
-    type: DataTypes.STRING,
-    primaryKey: true,
+const User = sequelize.define(
+  'User',
+  {
+    user_id: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
   },
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  role: {
-    type: DataTypes.BOOLEAN,
-    defaultValue: false,
-  },
-}, {
-  tableName: 'user',
-  timestamps: false,
-});
+  {
+    tableName: 'user',
+    timestamps: false,
+  }
+);
 
-module.exports = User; // Export User model and createUser function
+module.exports = User;
--- backend/routes/transcription.js
+++ backend/routes/transcription.js
@@ -4,11 +4,12 @@ const router = express.Router();
 const transcriptionController = require('../controllers/transcription');
 const { verifyToken } = require('../middlewares/auth');
 
-// Route to create a new transcription
+router.get('/', verifyToken, transcriptionController.getUserTranscriptions);
 router.post('/', verifyToken, transcriptionController.createTranscription);
-router.post('/save', verifyToken, transcriptionController.saveTranscription); // Use transcriptionController
 
-// Route to get all transcriptions of the logged-in user
-router.get('/', verifyToken, transcriptionController.getUserTranscriptions); // Use transcriptionController
+router.get('/:id', transcriptionController.getTranscription);
+router.delete('/:id', verifyToken, transcriptionController.deleteTranscription);
+
+router.post('/save', verifyToken, transcriptionController.saveTranscription);
 
 module.exports = router;
